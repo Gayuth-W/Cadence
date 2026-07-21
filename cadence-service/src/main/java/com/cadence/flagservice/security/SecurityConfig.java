1d80ba3030e2e6d1b304107f9b901ee40c86543c
Gayuth-W <gayuthdemika@gmail.com>
2026-07-21 15:40:14 +0530

    feat: cors configurations

--- cadence-service/src/main/java/com/cadence/flagservice/security/SecurityConfig.java
+++ cadence-service/src/main/java/com/cadence/flagservice/security/SecurityConfig.java
@@ -133,4 +133,16 @@ public class SecurityConfig {
     public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
         return config.getAuthenticationManager();
     }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOriginPatterns(List.of("http://localhost:*"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
 }
